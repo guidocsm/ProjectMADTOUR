df925d96f8c37309ba1ff5a377178e8b68aa01d7
marferbl <fernandezblascomarcos@gmail.com>
2021-11-22 10:12:55 +0100

    DONE

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,14 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
+    // var titleParallax = document.querySelector(".title-parallax");
+    // var titleParallax2 = document.querySelector(".title-parallax2");
+    window.addEventListener("scroll", function () {
+      // var value = window.scrollY;
+      // titleParallax.style.left = value * 0.85;
+      // titleParallax2.style.right = value * 0.85;
+    });
+
     console.log("ProjectMADTOUR JS imported successfully!");
   },
   false
