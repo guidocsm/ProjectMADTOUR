94fe1a01c2e8a8dc24b392870b8daa10a81e05fe
guidocsm <guidoignaciocrespo@gmail.com>
2021-11-22 15:32:48 +0100

    anothe fuck*ng change

--- routes/ent.routes.js
+++ routes/ent.routes.js
@@ -6,7 +6,8 @@ router.get("/all-ent", (req, res) => {
     Ent.find()
 
     .then( ents => res.render("entertainment/all-ent", { ents }))
-})
+});
+
 
 router.get("/create", (req, res) => {
 
@@ -16,15 +17,54 @@ router.get("/create", (req, res) => {
     });
 });
 
+
 router.post("/create", (req, res) => {
-    const { name, type, music, musicType, price, review, location, webSite, openingTime, closingTime } = req.body;
-   
-    Ent.create({ name, type, music, musicType, price, review, location, webSite, openingTime, closingTime })
-      .then(() => {
-        res.redirect("/entertainment/all-ent");
-      })
-      .catch((err) => console.log(err));
+  
+  // res.json(req.body)
+  const { name, type, music, musicType, price, review, location, webSite, openingTime, closingTime } = req.body;
+  
+  Ent.create({ name, type, music, musicType, price, review, location, webSite, openingTime, closingTime })
+  .then(() => {
+    res.redirect("/ent/all-ent");
+  })
+  .catch((err) => console.log(err));
 });
+
+
+router.get("/details/:id", (req, res) => {
+  const { id } = req.params
+
+  Ent.findById(id)    
+  .then( ent => res.render("entertainment/details", ent ))
+})
+
+
+router.get("/edit/:id", (req, res) => {
+  const { id } = req.params
+  
+  Ent.findById( id )
+  .then((editEnt) => {
+      res.render("entertainment/edit", {editEnt})})
+  .catch((err) => console.log(err))
+})
+
+
+router.post("/edit/:id", (req, res) => {
+  const { id } = req.params
+  const { name, type, music, musicType, price, review, location, webSite, openingTime, closingTime } = req.body
+
+  Ent.findByIdAndUpdate(id, { name, type, music, musicType, price, review, location, webSite, openingTime, closingTime })
+  .then(() => res.redirect("/ent/all-ent"))
+})
+
+
+router.get("/:id/delete", (req, res) => {
+  const { id } = req.params
+
+  Ent.findByIdAndRemove(id)
+  .then(() => res.redirect("/ent/all-ent"));
+});
+
 
 
 
