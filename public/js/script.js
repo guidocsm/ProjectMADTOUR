13a7d666596cf3fe6d3ba82a370a9f0c89552f86
marferbl <fernandezblascomarcos@gmail.com>
2021-11-24 20:27:15 +0100

    done

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,19 @@ document.addEventListener(
   "DOMContentLoaded",
   () => {
    
-    var phoneLogo = document.querySelector(".logoMAD");
+    var parallaxRestaurant = document.querySelector(".parallaxRestaurant");
+     var parallaxMonument = document.querySelector(".parallaxMonument");
+     var parallaxMuseum = document.querySelector(".parallaxMuseum");
+     var parallaxClub = document.querySelector(".parallaxClub");
+
+    
     window.addEventListener("scroll", function () {
       var value = window.scrollY;
-      phoneLogo.style.right = value * 0.65;
+      
+      parallaxRestaurant.style.left = value * 0.55;
+      parallaxMuseum.style.left = value * 0.78;
+      parallaxMonument.style.right = value * 0.75;
+       parallaxClub.style.right = value * 0.78;
      
     });
 
