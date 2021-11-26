77b4f4c9f729daae6378bf24bb62afdf97bd8cf4
marferbl <fernandezblascomarcos@gmail.com>
2021-11-26 09:59:14 +0100

    session config connected to mongo remote db

--- config/session.config.js
+++ config/session.config.js
@@ -5,17 +5,17 @@ const mongoose = require('mongoose');
 module.exports = app => {
     app.set('trust proxy', 1);
     app.use(
-        session({
-            secret: process.env.SESS_SECRET,
-            resave: true,
-            saveUninitialized: false,
-            cookie: {
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-                secure: process.env.NODE_ENV === 'production',
-                httpOnly: true,
-                maxAge: 60000 * 10000000000
-            },
-            store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI })
-        })
+      session({
+        secret: process.env.SESS_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        cookie: {
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+          secure: process.env.NODE_ENV === "production",
+          httpOnly: true,
+          maxAge: 60000 * 10000000000,
+        },
+        store: MongoStore.create({ mongoUrl: process.env.DB_REMOTE }),
+      })
     );
 };
